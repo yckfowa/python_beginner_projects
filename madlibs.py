128f43d3b0b2b9d63aa64e90cc67531ffa962d6a
yckfowa <80372122+yckfowa@users.noreply.github.com>
2021-06-19 13:02:03 +0800

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+print("Welcome to Mad Lib Generator! ")
+
+adj1 = input("Enter an adjective that describes the weather: ")
+adj2 = input("Enter an adjective to describe a thing: ")
+noun1 = input("Enter a place: ")
+noun2 = input("Enter a noun: ")
+noun3 = input("Enter a celebrity: ")
+noun4 = input("Enter a type of weapon ")
+
+
+print(f"Today is a {adj1} day... while me and my parents are heading to {noun1}, there was a {adj2} {noun2} blocking our way, as we proceed to move on, {noun3} comes in to save the day with a {noun4} !")
